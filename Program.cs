56330894c4706b53525254a1f75d50e035078f3f
nathansabin <sabinnat001@gmail.com>
2023-09-21 20:12:15 -0700

    practiced simple c# logic

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tester 
+{
+    class tester 
+    {
+        static void Main() 
+        {
+            int numOne = GetNum();
+            int numTwo = GetNum();
+            Console.WriteLine(Add(numOne, numTwo));
+        }
+        static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+        static int GetNum()
+        {
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
+        }
+    }
+}
